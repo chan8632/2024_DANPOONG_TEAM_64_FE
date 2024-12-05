164d2e61900524e3e65cc26312c1e93fc6e2d7cc
chan8632 <duscks8632@gmail.com>
2024-12-05 16:16:53 +0900

    타이머 기능(근데 다음날 기반이 안되는듯)

--- HomeScreen.jsx
+++ HomeScreen.jsx
@@ -1,6 +1,6 @@
 import dayjs from "dayjs";
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
   FlatList,
   StyleSheet,
@@ -16,7 +16,8 @@ import MsLogo from "./assets/Logo=Microsoft.svg";
 import NvidiaLogo from "./assets/Logo=Nvidia.svg";
 import MainLogo from "./assets/홈스크린/주식 한입 작은버전.svg";
 import { useSelector } from "react-redux";
-
+import duration from "dayjs/plugin/duration"; // 플러그인 가져오기
+dayjs.extend(duration); // 플러그인 활성화
 const HomeScreen = ({ navigation }) => {
   const currentScore = useSelector((state) => state.score.currentScore);
   const stocks = [
@@ -57,6 +58,45 @@ const HomeScreen = ({ navigation }) => {
     },
     { id: "6", name: "메타", price: "$228.02", logo: MetaLogo, ticker: "META" },
   ];
+  const [remainingTime, setRemainingTime] = useState("");
+
+  const calculateRemainingTime = () => {
+    const now = dayjs();
+    const startTime = dayjs().hour(16).minute(0).second(0);
+    const endTime = dayjs().hour(16).minute(30).second(0);
+
+    if (now.isBefore(startTime)) {
+      //현재 시각이 오후 11시 이전이면 타이머 멈춤
+      return "시작 전";
+    } else if (now.isBefore(endTime)) {
+      //현재 시각이 오후 11시 이후 오전 6시 이전
+      const duration = endTime.diff(now);
+      return formatDuration(duration);
+    } else {
+      //현재 시각이 오후 11시 이후(타이머 종료 상태)
+      return "시작 전";
+    }
+  };
+
+  const formatDuration = (duration) => {
+    const hours = Math.floor(dayjs.duration(duration).asHours());
+    const minutes = Math.floor(dayjs.duration(duration).asMinutes() % 60);
+    const seconds = Math.floor(dayjs.duration(duration).asSeconds() % 60);
+    return `${hours.toString().padStart(2, "0")}:${minutes
+      .toString()
+      .padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+  };
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      const time = calculateRemainingTime();
+      setRemainingTime(time);
+      if (time === "시작 전") {
+        return;
+      }
+    }, 1000);
+    return () => clearInterval(timer);
+  }, []);
 
   return (
     <View style={styles.container}>
@@ -91,9 +131,13 @@ const HomeScreen = ({ navigation }) => {
         </View>
         <View style={styles.cardWrapper}>
           <View style={styles.card}>
-            <Text style={styles.cardText}>{dayjs().format("HH:mm:ss")}</Text>
+            <Text style={styles.cardText}>{remainingTime}</Text>
           </View>
-          <Text style={styles.cardSubtext}>남은 시간 안에 맞춰보세요!</Text>
+          <Text style={styles.cardSubtext}>
+            {remainingTime === "시작 전"
+              ? "시작 전"
+              : "남은 시간 안에 맞춰보세요!"}
+          </Text>
         </View>
       </View>
       <Text style={styles.subTitle}>주식을 골라주세요!</Text>
